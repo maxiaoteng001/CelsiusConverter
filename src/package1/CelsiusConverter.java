c98730df47a944431784f5526d89a456d0b5a6f1
maxiaoteng001 <jizhuwo00@Hotmail.com>
2017-06-03 21:04:05 +0800

    调整了窗口组件的大小位置和变量名称

--- src/package1/CelsiusConverter.java
+++ src/package1/CelsiusConverter.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class CelsiusConverter extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField tempTextField;
 
 	/**
 	 * Launch the application.
@@ -31,12 +35,29 @@ public class CelsiusConverter extends JFrame {
 	 * Create the frame.
 	 */
 	public CelsiusConverter() {
+		setTitle("\u6E29\u5EA6\u8F6C\u6362\u5668");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 249, 184);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		tempTextField = new JTextField();
+		tempTextField.setBounds(23, 25, 66, 21);
+		contentPane.add(tempTextField);
+		tempTextField.setColumns(10);
+		
+		JLabel celsiusLabel = new JLabel("Celsius");
+		celsiusLabel.setBounds(115, 28, 79, 15);
+		contentPane.add(celsiusLabel);
+		
+		JButton convertButton = new JButton("Convert");
+		convertButton.setBounds(10, 65, 93, 23);
+		contentPane.add(convertButton);
+		
+		JLabel fahrenheitLabel = new JLabel("Fahrenheit");
+		fahrenheitLabel.setBounds(115, 69, 93, 15);
+		contentPane.add(fahrenheitLabel);
 	}
-
 }
